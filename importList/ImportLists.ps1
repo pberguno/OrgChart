9d2314d418c8a90cd7491b1bdc42bb0393637251
pberguno <pablobergunodiaz@gmail.com>
2017-02-16 18:04:24 +0100

    Añadiendo archivo powershell para automatizar la importación de listas.

---
+++ importList/ImportLists.ps1
@@ -0,0 +1,7 @@
+Add-PsSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | Out-Null 
+
+$url = "http://intranet.continental.com"
+
+Import-SPWeb $url -Path c:\backup\departamentos.cmp -UpdateVersions OverWrite
+Import-SPWeb $url -Path c:\backup\puestos.cmp -UpdateVersions OverWrite
+Import-SPWeb $url -Path c:\backup\empleados.cmp -UpdateVersions OverWrite
