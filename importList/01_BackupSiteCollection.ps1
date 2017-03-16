14e71d1003338992c409e1734a654149adaf25fa
pberguno <pablobergunodiaz@gmail.com>
2017-03-16 18:06:35 +0100

    Numerando scripts PowerShell.

--- importList/01_BackupSiteCollection.ps1
+++ importList/01_BackupSiteCollection.ps1
@@ -1,3 +1,5 @@
 $url = "http://contitest:19531/"
 
 Backup-SPSite $url -Path C:\temp\importList\siteCollectionBackup\contiSiteCollection.bak
+
+Write-Output "Copia de seguridad de la colección de sitios finalizada."
--- importList/ExportList.ps1
+++
@@ -1,6 +0,0 @@
-export-spweb -identity http://portal.intermark.com -itemurl /lists/Departments -path C:\Backups\Continental\Export\Departments.cmp
-export-spweb -identity http://portal.intermark.com -itemurl /lists/PositionTypes -path C:\Backups\Continental\Export\PositionTypes.cmp
-export-spweb -identity http://portal.intermark.com -itemurl /lists/Employees -path C:\Backups\Continental\Export\Employees.cmp
-export-spweb -identity http://portal.intermark.com -itemurl /EmployeePhotos -path C:\Backups\Continental\Export\EmployeePhotos.cmp
-
-Write-Output "Exportacion finalizada."
